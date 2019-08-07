2586ed90f2230de81057c0105540911ff732b44e
zqzal <936952020@qq.com>
2019-08-07 11:40:02 +0800

    数据库添加数据

--- app/src/main/java/com/xuxi/databasestest/MainActivity.java
+++ app/src/main/java/com/xuxi/databasestest/MainActivity.java
@@ -2,7 +2,9 @@ package com.xuxi.databasestest;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.ContentValues;
 import android.content.SharedPreferences;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -22,6 +24,30 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 dbHelper.getWritableDatabase();
+            }
+        });
+
+
+        Button addButton = findViewById(R.id.add_data);
+        addButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                SQLiteDatabase db = dbHelper.getWritableDatabase();
+                ContentValues values = new ContentValues();
+                //开始组装第一条数据
+                values.put("name","The Da Vinci Code");
+                values.put("author","Dan Brown");
+                values.put("pages",454);
+                values.put("price",16.96);
+                db.insert("Book",null,values); //插入第一条数据
+                values.clear();
+                //开始组装第二条数据
+                values.put("name","The Lost Symbol");
+                values.put("author","Dan Brown");
+                values.put("pages",510);
+                values.put("price",19.95);
+                db.insert("Book",null,values); //插入第二条数据
             }
         });
 
